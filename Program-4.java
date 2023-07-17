9d92891a71bd07a2295b0fdc5b350c7bbdb63482
apoorvajt <139798867+apoorvajt@users.noreply.github.com>
2023-07-17 23:17:32 +0530

    Create Program-4.java

---
+++ Program-4.java
@@ -0,0 +1,26 @@
+package Program
+import java.util.*;
+class OccuranceInteger{
+@SuppressWarnings("unchecked")
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int count;
+int n=sc.nextInt();
+int arr[]=new int[n];
+Map<Integer,Integer> map=new HashMap();
+for(int i=0;i<n;i++){
+arr[i]=sc.nextInt();}
+for(int i=1;i<=9;i++){
+count=0;
+for(int k=0;k<arr.length;k++){
+if(arr[k]%i==0){
+count++;
+}
+
+map.put(i,count);
+
+}
+}
+System.out.println(map);
+}
+}
